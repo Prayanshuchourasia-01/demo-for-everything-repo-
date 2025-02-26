8285a58f9ac84a85c8d8ecfc1934772e2c980353
PrayanshuChourasia <prayanshuchourasia01@gmail.com>
2025-02-26 16:55:10 +0530

    js file

---
+++ index.js
@@ -0,0 +1,33 @@
+const firstNumber = {
+    Name : " " , 
+    Email: " ",
+    phoneNumber : " " , 
+
+}
+const secondNumber = {
+    Name : "gfggygf" , 
+    Email: "prghg@gmail.com",
+    phoneNumber : "1237894560" , 
+
+}
+const thirdNumber = {
+    Name : "world " , 
+    Email: "world@gmail.com ",
+    phoneNumber : "4567891230" , 
+
+}
+const forthNumber = {
+    Name : " Hello  " , 
+    Email: "hello@gmail.com",
+    phoneNumber : "1234567890" , 
+
+}
+
+
+
+
+
+
+
+
+
